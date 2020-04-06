fbbb7d28d55a424f454e856d66ad2c787165ef6d
aliaa80 <a.ansari80@outlook.com>
2020-04-06 23:27:42 +0430

    Add print methode

--- src/Card.java
+++ src/Card.java
@@ -13,7 +13,8 @@ public class Card {
 
     /**
      * Constructor for Card
-     * @param color Color of card
+     *
+     * @param color  Color of card
      * @param number number of card
      */
     public Card(String color, int number) {
@@ -40,7 +41,25 @@ public class Card {
     }
 
     public void print() {
-        System.out.println(color+number);
+        String ANSI_BLUE = "\u001B[34m";
+        String ANSI_GREEN = "\u001B[32m";
+        String ANSI_RED = "\u001B[31m";
+        String ANSI_YELLOW = "\u001B[33m";
+        String ANSI_BLACK = "\u001B[30m";
+        if (color.equals("blue")) System.out.print(ANSI_BLUE);
+        else if (color.equals("green")) System.out.print(ANSI_GREEN);
+        else if (color.equals("red")) System.out.print(ANSI_RED);
+        else if (color.equals("yellow")) System.out.print(ANSI_YELLOW);
+        else System.out.print(ANSI_BLACK);
+        System.out.print("###########\n|         |\n|");
+        if(number<10) System.out.print("    "+number+"    ");
+        else if(number==10) System.out.print(" S K I P ");
+        else if(number==11) System.out.print("  R E V  ");
+        else if(number==12) System.out.print("   + 2   ");
+        else if(number==13) System.out.print("   + 4   ");
+        else if(number==14) System.out.print("C O L O R");
+        System.out.println("|\n|         |\n###########");
     }
+
 }
 
