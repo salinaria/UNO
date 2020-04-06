d91f5382532ee9b3622151855cca1ab1ffed8bc2
aliaa80 <a.ansari80@outlook.com>
2020-04-06 23:31:00 +0430

    Add methods and develop main game

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
     private int numPlayers;
@@ -60,7 +61,88 @@ public class Game {
             }
             players.add(player);
         }
+        Scanner sc=new Scanner(System.in);
+        int j=1;
+        for(Player i:players){
+            System.out.print("Player "+j+"name:");
+            i.setName(sc.next());
+            j++;
+        }
+    }
+    public boolean endGame(){
+        for(Player i:players){
+            if(i.numCard()==0)return true;
+        }
+        return false;
+    }
+    public void reverse(int turn,int changer,Card card){
+        players.get(Math.abs(turn)%numPlayers).removeCard(card);
+        changer=changer*-1;
+    }
+    public void skip(int turn,Card card){
+        players.get(Math.abs(turn)%numPlayers).removeCard(card);
+        turn++;
+    }
+    public void plus2(int turn,Card card){
+        players.get(Math.abs(turn)%numPlayers).removeCard(card);
+        for(int j=0;j<2;j++){
+            int rand1=(int) (Math.random()*(cards.size()-1));
+            players.get(Math.abs(turn+1)%numPlayers).addCard(cards.get(rand1));
+            cards.remove(rand1);
+        }
+    }
+    public String plus4(int turn,Card card){
+        players.get(Math.abs(turn)%numPlayers).removeCard(card);
+        for(int j=0;j<4;j++){
+            int rand1=(int) (Math.random()*(cards.size()-1));
+            players.get(Math.abs(turn+1)%numPlayers).addCard(cards.get(rand1));
+            cards.remove(rand1);
+        }
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Choose your color");
+        return sc.next();
+    }
+    public String changeColor(int turn,Card card){
+        players.get(Math.abs(turn)%numPlayers).removeCard(card);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Choose your color");
+        return sc.next();
     }
     public void game(){
+        start();
+        int turn=0;
+        int changer=1;
+        int rand=(int) (Math.random()*(cards.size()-1));
+        cards.remove(rand);
+        Card card=cards.get(rand);
+        while (!endGame()){
+            card.print();
+            players.get(Math.abs(turn%numPlayers)).print();
+            if(!players.get(Math.abs(turn%numPlayers)).availableCard(card)) {
+                System.out.println("You don't have any cards to choose it we will get you a card Are you ok?(1.Yes 2.No)");
+                Scanner sc = new Scanner(System.in);
+                sc.nextInt();
+                rand = (int) (Math.random() * (cards.size() - 1));
+                cards.remove(rand);
+                players.get(Math.abs(turn%numPlayers)).addCard(cards.get(rand));
+            }
+            if(players.get(Math.abs(turn%numPlayers)).availableCard(card)){
+                System.out.print("Your choose:");
+                Scanner sc=new Scanner(System.in);
+                int choose=sc.nextInt()-1;
+                card=players.get(Math.abs(turn%numPlayers)).getCards().get(choose);
+                if(players.get(Math.abs(turn%numPlayers)).getCards().get(choose).getNumber()==10)skip(turn,players.get(Math.abs(turn%numPlayers)).getCards().get(choose));
+                else if(players.get(Math.abs(turn%numPlayers)).getCards().get(choose).getNumber()==11)reverse(turn,changer,players.get(Math.abs(turn%numPlayers)).getCards().get(choose));
+                else if(players.get(Math.abs(turn%numPlayers)).getCards().get(choose).getNumber()==12)plus2(turn,players.get(Math.abs(turn%numPlayers)).getCards().get(choose));
+                else if(players.get(Math.abs(turn%numPlayers)).getCards().get(choose).getNumber()==13){
+                    card=new Card(plus4(turn,players.get(Math.abs(turn%numPlayers)).getCards().get(choose)),13);
+                }
+                else if(players.get(Math.abs(turn%numPlayers)).getCards().get(choose).getNumber()==14){
+                    String color=changeColor(turn,players.get(Math.abs(turn%numPlayers)).getCards().get(choose));
+                    card=new Card(color,14);
+                }
+            }
+            turn=turn+changer;
+        }
     }
 }
